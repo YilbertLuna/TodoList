87c404a3e18303fe911eb5d8f4d29b3964a78053
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-05-14 22:28:02 -0400

    adding components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { CreateTodo } from "./components/CreateTodo";
+import { CounterTodo } from "./components/CounterTodo";
+import { ListTodo } from "./components/ListTodo";
+import { TodoItem } from "./components/TodoItem";
+
 
 function App() {
   return (
     <div>
+
+      <CreateTodo/>
+
+      <CounterTodo/>
+
+      <ListTodo>
+        <TodoItem/>
+      </ListTodo>
+
     </div>
   );
 }
