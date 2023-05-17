c175fa490b3e8cca2c5ed07b49d520ac856bc4ee
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-05-17 14:35:18 -0400

    add styles, onSubmit event and functionality to the textarea

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import '../style/CreateTodo.css'
 
 function CreateTodo() {
-    return(
-        <form>
-            <textarea/>
 
-            <button>Crear</button>
+    const [todoValue, setTodoValue] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const onChangeValue = (e) => {
+        setTodoValue(e.target.value)
+    }
+
+    return(
+        <form onSubmit={onSubmit} className="form-create-todo">
+            <textarea
+            className="text-create-todo"
+            placeholder="Crea una nueva tarea por hacer"
+            value={todoValue}
+            onChange={onChangeValue}
+            />
+
+            <button
+            className="button-create-todo"
+            disabled={todoValue ? '' : 'disabled'}
+            >Crear</button>
         </form>
     )
 }
