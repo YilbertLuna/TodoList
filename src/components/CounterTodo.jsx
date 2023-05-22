ef220e5d94674a5544408fb7b14935e1695b51af
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-05-22 19:22:55 -0400

    modified file

--- src/components/CounterTodo.jsx
+++ src/components/CounterTodo.jsx
@@ -3,7 +3,10 @@ import '../style/CounterTodo.css'
 
 function CounterTodo({ total, completed }) {
     return(
-        <p className="counter-todo">tienes {total} tareas, y haz completado {completed}</p>
+        <>
+            <p className="counter-todo">Tareas: {total}</p>
+            <p className="counter-todo">Completadas: {completed}</p>
+        </>
     )
 }
 
