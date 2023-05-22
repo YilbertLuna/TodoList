1ca2817daa0ae3bc749477262a3b747397b5d41c
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-05-22 14:22:11 -0400

    completeTodos and deleteTodos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import '../style/TodoItem.css'
 
-function TodoItem({ text, completed }) {
+function TodoItem({ toggleCompletedTodos, deleteTodo, text, completed }) {
+
     return(
         <li className="Todo-Item-List">
 
-            <button className="todo-completed" type="">✔️</button>
+            <button className="todo-completed" onClick={toggleCompletedTodos}>✔️</button>
 
             <p className={`item-list ${completed && "TodoItem-p--complete"}`}>{ text }</p>
 
-            <button className="todo-deteled">❌</button>
+            <button className="todo-deteled" onClick={deleteTodo}>❌</button>
 
         </li>
     )
